8122ae8c2b7302aee870174f946d044d83bbf6eb
nannn <2337286751@qq.com>
2022-07-03 01:03:57 +0800

    7/3 ts study

---
+++ type-challenges/00116-medium-replace/template.ts
@@ -0,0 +1,5 @@
+type Replace<S extends string, From extends string, To extends string> = From extends ''
+  ? S
+  : S extends `${infer B}${From}${infer E}`
+  ? `${B}${To}${E}`
+  : S;
---
+++ type-challenges/00119-medium-replaceall/template.ts
@@ -0,0 +1,5 @@
+type ReplaceAll<S extends string, From extends string, To extends string> = From extends ''
+  ? S
+  : S extends `${infer B}${From}${infer E}`
+  ? `${B}${To}${ReplaceAll<E, From, To>}`
+  : S;
